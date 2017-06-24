4128f0eccbf3ab58996a6b99ccac3e8b8daf1269
mariomelchor <mario@mariomelchor.com>
2017-06-23 23:20:43 -0700

    Added normalize, added css styles, added function to check guess

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,37 @@
 window.onload = function () {
 
-  // Define variables
+  // Define variables/arrays
   var wins = 0;
   var losses = 0;
-
-  // Create Words Array
+  var guesses = 10;
   var words = ['Niva' , 'Mario', 'Rosa', 'Matthew', 'Angel'];
-
-  // Create Alphabet Array
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   // Randomly chooses a word from the words array.
   var randomWord = words[Math.floor(Math.random() * words.length)];
-  // console.log(randomWord);
+  randomWord = randomWord.toLowerCase();
 
-  // Create Alphabet List Dynamically
+  // Create a div for each letter in randomWord
+  var wordsList = function () {
+    var wordWrap = document.getElementById('word-wrap');
+
+    for ( var i = 0; i < randomWord.length; i++ ) {
+      var wordLetter = document.createElement('div');
+      var character = randomWord.charAt(i);
+      // console.log(character);
+
+      wordLetter.id = 'word-letter-' + character;
+      wordLetter.className = 'word-letter word-letter-' + character;
+      // wordLetter.innerHTML = character;
+      wordWrap.appendChild(wordLetter);
+
+    }
+  }
+
+  // Call function
+  wordsList();
+
+  // Create a div for each letter in alphabet array
   var alphabetList = function () {
     var alphabetWrap = document.getElementById('alphabet-wrap');
 
@@ -27,47 +44,53 @@ window.onload = function () {
     }
   }
 
-  // Call function that writes alphabetList to DOM
+  // Call function
   alphabetList();
 
-  // Get all elements with class letter
+  // Get all elements with class letter loop through track click
   var alphabetLetter = document.getElementsByClassName('letter');
-
-  // Loop all elements with class letter and log clicked
   for ( var i = 0; i < alphabetLetter.length; i++ ) {
       alphabetLetter[i].onclick = function() {
-      console.log(this.innerHTML);
-      this.classList.add('active');
+
+      letterClicked = this.innerHTML;
+      checkGuess( letterClicked, randomWord );
+
     }
   }
-
-  // Create Word List Dynamically
-  var wordsList = function () {
-    var wordWrap = document.getElementById('word-wrap');
-
-    for ( var i = 0; i < randomWord.length; i++ ) {
-      var wordLetter = document.createElement('div');
-      var character = randomWord.charAt(i);
-
-      wordLetter.id = 'word-letter-' + character;
-      wordLetter.className = 'word-letter';
-      // wordLetter.innerHTML = character;
-      wordWrap.appendChild(wordLetter);
-
-      console.log(randomWord.charAt(i));
-    }
-  }
-
-  // Call function that writes word List to DOM
-  wordsList();
 
   // Get which key was pressed
   document.onkeypress = function(e) {
      var keyPressed = String.fromCharCode(e.keyCode);
      // console.log(keyPressed );
 
-     document.getElementById('letter-' + keyPressed ).classList.add('active');
-  };
+     checkGuess( keyPressed, randomWord );
+
+  }
+
+  // Checks if letter exist in word
+  function checkGuess( guess, word ) {
+
+    document.getElementById('letter-' + guess ).classList.add('active');
+
+    if ( word.indexOf( guess ) > -1 ) {
+       console.log( 'Yes ' + guess );
+       // console.log( word.indexOf( guess ) );
+
+       letter = document.getElementById('word-letter-' + guess );
+       letter.innerHTML = guess;
+    } else {
+       console.log( 'No ' + guess );
+       guesses--
+       console.log( "Guesses Remaining " + guesses );
+    }
+
+    if ( guesses === 0 ) {
+      alert('You Lose');
+      losses++
+      guesses = 10;
+      console.log( "Total Losses " + losses );
+    }
+  }
 
 
 }
