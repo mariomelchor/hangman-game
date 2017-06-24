883a8e6a3139458313a000f2be4bc2c298e4dfb0
mariomelchor <mario@mariomelchor.com>
2017-06-23 20:36:00 -0700

    Added word list to display

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
 window.onload = function () {
 
+  // Define variables
+  var wins = 0;
+  var losses = 0;
+
+  // Create Words Array
+  var words = ['Niva' , 'Mario', 'Rosa', 'Matthew', 'Angel'];
+
   // Create Alphabet Array
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+  // Randomly chooses a word from the words array.
+  var randomWord = words[Math.floor(Math.random() * words.length)];
+  // console.log(randomWord);
+
   // Create Alphabet List Dynamically
   var alphabetList = function () {
-    alphabetWrap = document.getElementById('alphabet-wrap');
+    var alphabetWrap = document.getElementById('alphabet-wrap');
 
     for ( var i = 0; i < alphabet.length; i++ ) {
-      letter = document.createElement('div');
+      var letter = document.createElement('div');
       letter.id = 'letter-' + alphabet[i];
       letter.className = 'letter';
       letter.innerHTML = alphabet[i];
@@ -30,10 +41,30 @@ window.onload = function () {
     }
   }
 
+  // Create Word List Dynamically
+  var wordsList = function () {
+    var wordWrap = document.getElementById('word-wrap');
+
+    for ( var i = 0; i < randomWord.length; i++ ) {
+      var wordLetter = document.createElement('div');
+      var character = randomWord.charAt(i);
+
+      wordLetter.id = 'word-letter-' + character;
+      wordLetter.className = 'word-letter';
+      // wordLetter.innerHTML = character;
+      wordWrap.appendChild(wordLetter);
+
+      console.log(randomWord.charAt(i));
+    }
+  }
+
+  // Call function that writes word List to DOM
+  wordsList();
+
   // Get which key was pressed
   document.onkeypress = function(e) {
      var keyPressed = String.fromCharCode(e.keyCode);
-     console.log(keyPressed );
+     // console.log(keyPressed );
 
      document.getElementById('letter-' + keyPressed ).classList.add('active');
   };
